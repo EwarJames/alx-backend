ce01d277a71b1e32fb3d344ce6767fdbd512802d
EwarJames <ewaarjames@gmail.com>
2021-08-13 18:49:37 +0300

    Basic cache system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Implements Python caching"""
+from base_caching import BaseCaching
+
+
+def BasicCache(BaseCaching):
+    """Dictinary cache"""
+
+    def put(self, key, item):
+        """Puts items into the cache"""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data(key) = item
+
+    def get(self, key):
+        """Gets an item by key"""
+        return self.cache_data.get(key)
